c50cf33a752bab36e2b4d65ad0e9807aa5e15037
yosebyte <id106@duck.com>
2026-06-23 05:00:51 +0000

    Fix connection handling in NowhereTCPConnectionPool

--- Shared/Networking/Protocols/Core/ProxyConfiguration.swift
+++ Shared/Networking/Protocols/Core/ProxyConfiguration.swift
@@ -252,6 +252,7 @@ struct ProxyConfiguration: Identifiable, Hashable, Codable {
     }
 
     var displaySecurityTag: String? {
+        guard outboundProtocol != .nowhere else { return nil }
         let tag = (outboundProtocol == .vless
             ? xraySecurityLayer.tag
             : genericSecurityLayer.tag).uppercased()
